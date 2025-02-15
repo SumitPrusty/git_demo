043ec41806d34c17e3861b35a1a2b1213a8a7116
SumitPrusty <sumitprusty85@gmail.com>
2025-02-15 16:09:47 +0530

    User entity Created

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+public class User {
+    int id;
+    String userName;
+    int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
